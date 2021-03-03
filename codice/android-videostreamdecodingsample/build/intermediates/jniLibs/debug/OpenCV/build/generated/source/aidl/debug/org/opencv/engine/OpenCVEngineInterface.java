009a058b7033b6b299b92bfe724ff74642bd222e
Riccardo Lastrico <riky.lastrico@gmail.com>
2021-03-03 22:59:53 +0100

    change of library "./libs/armeabi-v7a/libffmpeg.so"

--- codice/android-videostreamdecodingsample/build/intermediates/jniLibs/debug/OpenCV/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ codice/android-videostreamdecodingsample/build/intermediates/jniLibs/debug/OpenCV/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\rick\\Desktop\\android-videostreamdecodingsample\\libs\\OpenCV\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\rick\\Desktop\\dronevideostream\\codice\\android-videostreamdecodingsample\\libs\\OpenCV\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
